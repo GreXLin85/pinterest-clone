d2906c5d1e19fadf8118ad46136a7306e8696191
GreXLin85 <grexlin85@gmail.com>
2023-01-18 21:19:03 +0200

    All source files moved to src folder

---
+++ server/src/seeder.ts
@@ -0,0 +1,49 @@
+import { Role } from "@prisma/client";
+import prisma from "./interfaces/Prisma";
+
+async function main() {
+    await prisma.role.createMany({
+        data: [
+            {
+                name: 'ADMIN',
+                permissions: ["READ_USER", "CREATE_USER", "UPDATE_USER"]
+            },
+            {
+                name: 'USER',
+                permissions: ["READ_USER"]
+            },
+        ]
+    });
+
+    const adminRole = await prisma.role.findUnique({
+        where: {
+            name: 'ADMIN'
+        }
+    }) as Role;
+
+    const userRole = await prisma.role.findUnique({
+        where: {
+            name: 'USER'
+        }
+    }) as Role;
+
+    await prisma.user.createMany({
+        data: [
+            {
+                username: 'Admin',
+                password: 'admin',
+                roleId: adminRole.id
+            },
+            {
+                username: 'User',
+                password: 'user',
+                roleId: userRole.id
+            }
+        ]
+    });
+
+    console.log('Seeding done');
+
+
+
+}
