b473100f4f9cc12fb21ca2ebb8c9163830b80b7f
GreXLin85 <grexlin85@gmail.com>
2023-01-19 09:14:44 +0200

    No need for index.ts

--- src/modules/auth/index.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "../../interfaces/Prisma";
-import { AuthController } from "./controller";
-import { AuthRoute } from "./route";
-import { AuthService } from "./services";
-
-let Auth = new AuthRoute()
-
-export default Auth
