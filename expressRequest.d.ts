c922c87ebd28655d521abfcc850c5ba9eb8d6185
GreXLin85 <grexlin85@gmail.com>
2023-01-23 20:34:16 +0200

    Types for express request

---
+++ expressRequest.d.ts
@@ -0,0 +1,14 @@
+declare global {
+    namespace Express {
+      interface Request {
+        user: {
+          id: number;
+        };
+      }
+  
+    }
+  }
+  
+  // If this file has no import/export statements (i.e. is a script)
+  // convert it into a module by adding an empty export statement.
+  export { }
