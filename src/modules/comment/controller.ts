fb72a0aab7000834ffd562429beaf8fe044e1759
GreXLin85 <grexlin85@gmail.com>
2023-01-30 20:50:30 +0200

    namings and class structure changed

--- src/modules/comment/controller.ts
+++ src/modules/comment/controller.ts
@@ -3,29 +3,31 @@ import MessageHelper from "../../helpers/MessageHelper";
 import { CommentService } from "./services";
 
 export class CommentController {
+    private commentService = new CommentService()
+
     async addCommentToPost(req: Request, res: Response) {
         const { postId, userId, content } = req.body;
-        const comment = await new CommentService().addCommentToPost(postId, userId, content);
-        if (comment) {
-            return MessageHelper(comment, false, res);
+        const commentAdded = await this.commentService.addCommentToPost(postId, userId, content);
+        if (commentAdded) {
+            return MessageHelper(commentAdded, false, res);
         }
         return MessageHelper("Something happened while adding comment", true, res);
     }
 
     async getCommentById(req: Request, res: Response) {
         const { id } = req.params;
-        const comment = await new CommentService().getCommentById(Number(id));
-        if (comment) {
-            return MessageHelper(comment, false, res);
+        const commentRead = await this.commentService.getCommentById(Number(id));
+        if (commentRead) {
+            return MessageHelper(commentRead, false, res);
         }
         return MessageHelper("Comment not found", true, res);
     }
 
     async getCommentsByPostId(req: Request, res: Response) {
         const { id } = req.params;
-        const comments = await new CommentService().getCommentsByPostId(Number(id));
-        if (comments) {
-            return MessageHelper(comments, false, res);
+        const commentsRead = await this.commentService.getCommentsByPostId(Number(id));
+        if (commentsRead) {
+            return MessageHelper(commentsRead, false, res);
         }
         return MessageHelper("Comments not found", true, res);
     }
@@ -33,18 +35,18 @@ export class CommentController {
     async editComment(req: Request, res: Response) {
         const { id } = req.params;
         const { content } = req.body;
-        const comment = await new CommentService().editComment(Number(id), content);
-        if (comment) {
-            return MessageHelper(comment, false, res);
+        const commentEdited = await this.commentService.editComment(Number(id), content);
+        if (commentEdited) {
+            return MessageHelper(commentEdited, false, res);
         }
         return MessageHelper("Something happened while editing comment", true, res);
     }
 
     async removeCommentFromPost(req: Request, res: Response) {
         const { id } = req.params;
-        const comment = await new CommentService().removeCommentFromPost(Number(id));
-        if (comment) {
-            return MessageHelper(comment, false, res);
+        const commentRemoved = await this.commentService.removeCommentFromPost(Number(id));
+        if (commentRemoved) {
+            return MessageHelper(commentRemoved, false, res);
         }
         return MessageHelper("Something happened while removing comment", true, res);
     }
